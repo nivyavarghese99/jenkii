587fcde1ce389cc479541b5a49d85298d16b0454
Nivya Varghese <Nivya Varghese@LAPTOP-VQMC9OSC>
2017-10-06 14:30:32 +1300

    committ

--- Jenki/src/main/java/Calcu.java
+++ Jenki/src/main/java/Calcu.java
@@ -5,6 +5,7 @@ public class Calcu {
 		public int substract(int x, int y) {
 			return x-y;
 			
+			
 				
 		}
 		public int multiply (int x, int y)
--- Jenki/src/test/java/CalcuTest.java
+++ Jenki/src/test/java/CalcuTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 public class CalcuTest {
 	int x,y;
 	Calcu obj;
+	
+	
 
 	@Before
 	public void setUp() throws Exception {
