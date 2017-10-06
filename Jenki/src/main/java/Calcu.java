2848fbdd50584aa8b352db1a1f8f9342fb57d4f9
Nivya Varghese <Nivya Varghese@LAPTOP-VQMC9OSC>
2017-10-06 14:27:50 +1300

    commiit-jenkii

---
+++ Jenki/src/main/java/Calcu.java
@@ -0,0 +1,24 @@
+
+public class Calcu {
+	
+	
+		public int substract(int x, int y) {
+			return x-y;
+			
+				
+		}
+		public int multiply (int x, int y)
+		{
+			return x*y;
+		}
+
+		public int addition (int x, int y)
+		{
+			return x+y;
+		}
+
+		
+
+	}
+
+
---
+++ Jenki/src/test/java/CalcuTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class CalcuTest {
+	int x,y;
+	Calcu obj;
+
+	@Before
+	public void setUp() throws Exception {
+
+		x=8;
+		y=4;		
+		obj= new Calcu();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		x=0;
+		y=0;		
+		obj=null;
+	}
+
+	@Test
+	public void testSubstract() {
+		assertEquals(4,obj.substract(x, y));
+	}
+
+	@Test
+	public void testMultiply() {
+		assertEquals(32,obj.multiply(x, y));
+	}
+
+	@Test
+	public void testAddition() {
+		assertEquals(12,obj.addition(x, y));
+	}
+
+}
